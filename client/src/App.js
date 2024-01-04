9869bd37b2c5f62298935adb0d491f1b70f2ff47
Anggoro10 <trianggoroi84@gmail.com>
2024-01-04 20:01:45 +0700

    set up

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Blabla from './component/Blabla';
+
+function App() {
+  return (
+    <div>
+      <Blabla />
+    </div>
+  );
+}
+
+export default App;
